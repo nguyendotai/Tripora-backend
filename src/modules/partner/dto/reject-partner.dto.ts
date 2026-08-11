11159154a8473eb5fc0049afda2f9aefde6f8dc0
nguyendotai <nguyendotai11@gmail.com>
2026-08-11 20:49:28 +0700

    Reset project cho roadmap moi (Tripora)

--- src/modules/partner/dto/reject-partner.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, MaxLength, MinLength } from 'class-validator';
-
-export class RejectPartnerDto {
-  @IsString()
-  @MinLength(1)
-  @MaxLength(500)
-  reason: string;
-}
--- src/modules/property/dto/reject-property.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, MaxLength, MinLength } from 'class-validator';
-
-export class RejectPropertyDto {
-  @IsString()
-  @MinLength(1)
-  @MaxLength(500)
-  reason: string;
-}
